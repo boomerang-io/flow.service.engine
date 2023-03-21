58544b4404c54dac1f085e600033ae09082775bd
Tyson Lawrie <tlawrie@users.noreply.github.com>
2023-03-21 14:10:51 +1100

    Add Sleep to TaskType

--- src/main/java/io/boomerang/model/enums/TaskType.java
+++ src/main/java/io/boomerang/model/enums/TaskType.java
@@ -1,5 +1,5 @@
 package io.boomerang.model.enums;
 
 public enum TaskType {
-  start, end, template, custom, generic, decision, approval, setwfproperty, manual, eventwait, acquirelock, releaselock, runworkflow, runscheduledworkflow, script, setwfstatus
+  start, end, template, custom, generic, decision, approval, setwfproperty, manual, eventwait, acquirelock, releaselock, runworkflow, runscheduledworkflow, script, setwfstatus, sleep
 }
