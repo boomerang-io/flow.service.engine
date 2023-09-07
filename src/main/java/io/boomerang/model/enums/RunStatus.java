523e50de4c034dfd2a5780eaff099ea4cafac63a
Tyson Lawrie <tlawrie@users.noreply.github.com>
2023-09-07 19:25:50 +1000

    Update shared RunStatus model

--- src/main/java/io/boomerang/model/enums/RunStatus.java
+++ src/main/java/io/boomerang/model/enums/RunStatus.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum RunStatus {
   notstarted("notstarted"), ready("ready"), running("running"), waiting("waiting"),  // NOSONAR
-  succeeded("succeeded"), failed("failed"), invalid("invalid"), skipped("skipped"), cancelled("cancelled"), timedout("timedout"); // NOSONAR
-
+  succeeded("succeeded"), failed("failed"), invalid("invalid"), skipped("skipped"), // NOSONAR 
+  cancelled("cancelled"), timedout("timedout"); // NOSONAR
+  
   private String status;
 
   RunStatus(String status) {
